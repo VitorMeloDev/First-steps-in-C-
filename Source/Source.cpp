85bc04abeefd1d31a6020d62940a03a3263ec3bb
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-04-16 10:32:27 -0300

    Create main game loop

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -1,5 +1,23 @@
+#include <raylib.h>
+
+#define SCREEN_X 1200
+#define SCREEN_Y 700
+#define WINDOW_TITLE "Game"
 
 int main()
 {
-    return 0;
+	InitWindow(SCREEN_X, SCREEN_Y, WINDOW_TITLE);
+	InitAudioDevice();
+
+	// main game loop
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(BLACK);
+		EndDrawing();
+	}
+	CloseAudioDevice();
+	CloseWindow();
+
+	return 0;
 }
