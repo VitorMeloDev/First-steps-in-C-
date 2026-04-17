c4dd8b90567b92950e1f20620102857d87d1e88c
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-04-17 08:17:14 -0300

    Created a struct to be an entity

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -1,4 +1,8 @@
 #include <raylib.h>
+#include <string>
+#include <array>
+#include <iostream>
+#include <vector>
 
 #define SCREEN_X 1200
 #define SCREEN_Y 700
@@ -9,6 +13,8 @@ struct Entity
 	Rectangle rec;
 	Color cor;
 	float speed = 50.f;
+	int life = 100;
+	bool collision = false;
 };
 
 void MovePlayer(Entity &player)
@@ -39,22 +45,30 @@ int main()
 	Entity player{ Rectangle{SCREEN_X / 2.f, SCREEN_Y / 2.f, 50.f, 50.f}, BLUE, 300.f };
 	Entity enemy{ Rectangle{SCREEN_X / 3.f, SCREEN_Y / 3.f, 50.f, 50.f}, RED, 0.f };
 
+	std::vector<Entity> enemies;
+	enemies.push_back(enemy);
 
 	// main game loop
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
+		DrawFPS(10, 10);
+		DrawText(std::to_string(player.life).c_str(), SCREEN_X - 100, 0, 32, WHITE);
 		MovePlayer(player);
 		DrawRectangle(player.rec.x , player.rec.y, player.rec.width, player.rec.height, player.cor);
-		DrawRectangle(enemy.rec.x, enemy.rec.y, enemy.rec.width, enemy.rec.height, enemy.cor);
+		DrawRectangle(enemies[0].rec.x, enemies[0].rec.y, enemies[0].rec.width, enemies[0].rec.height, enemies[0].cor);
 
-		if (CheckCollisionRecs(player.rec, enemy.rec))
+		if (CheckCollisionRecs(player.rec, enemies[0].rec))
 		{
-			player.cor = PURPLE;
+			if (!enemies[0].collision)
+			{
+				player.life -= 10;
+			}
+			enemies[0].collision = true;
 		}
 		else
 		{
-			player.cor = BLUE;
+			enemies[0].collision = false;
 		}
 
 		ClearBackground(BLACK);
