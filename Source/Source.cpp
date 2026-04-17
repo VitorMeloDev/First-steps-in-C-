4e150e856f0d53b8e1bf17e1f91c75b79b3b8ba8
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2026-04-17 09:29:49 -0300

    Basic game loop

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -1,7 +1,24 @@
 #include <raylib.h>
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	cout << "Starting the game" << endl;
+	const int screenWidth = 1200;
+	const int screenHeight = 700;
+	InitWindow(screenWidth, screenHeight, "Pong - VM Games");
+	SetTargetFPS(60);
+
+	while (WindowShouldClose() == false)
+	{
+		BeginDrawing();
+		DrawFPS(10, 10);
+		ClearBackground(BLACK);
+		EndDrawing();
+	}
+
+	CloseWindow();
 	return 0;
 }
